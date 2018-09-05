adef735499c372c7f2b9b59c47fb6e5670323b81
CUTYlife <2055305346@qq.com>
2018-09-05 17:00:13 +0800

    table.js异常机制，整个框架的骨架结构

--- js/table.js
+++ js/table.js
@@ -1,5 +1,5 @@
 /*
-* 框架开发的宗旨：方便快捷、性能优秀、兼容性高
+* 框架开发的宗旨：方便快捷、性能优秀、兼容性高、清晰明了的开发文档
 * 框架开发采用IIFE模式的原因：防止全局污染；降低代码的耦合度，向外只暴露
 * 需要暴露的内容。
 *
@@ -9,11 +9,58 @@
 * 传入，实际上在里面写也没有问题，但是从阅读性角度看，factory参数的方式要更加
 * 清晰
 * */
+//TODO 代码运行流程分析
 (function (global, factory) {
     //模块化判断,来自于Vue.js源码,判断之后，是如何进行引出的问题，就是向外
     // 暴露什么
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : (global.Table = factory());
 
 })(typeof window !== "undefined" ? window : this, function () {
+    //使用严格模式
+    'use strict';
+    //工具类  其他一些内容的编写位置
+    var typeUtil = {
 
+        isObject: function (obj){
+            return obj !== null && typeof obj === "object";
+        },
+
+        isString: function (arg) {
+            return typeof arg === "string";
+        },
+
+        isNull: function (arg) {
+            return arg === null;
+        },
+
+        isUndefined: function (arg) {
+            return arg === undefined;
+        }
+    };
+
+    /*
+    * obj:对象，形如
+    * {el:"#wrap",.....}
+    * */
+    var Table = function (obj) {
+        //框架需要有完善的报错体系
+        if (typeUtil.isUndefined(this)) {
+            throw new Error("请不要直接调用Table()函数");
+        }
+
+        if (!typeUtil.isObject(obj)) {
+            throw new Error("Table()构造函数必须传入对象作为参数");
+        }
+
+        if (!obj.el || !typeUtil.isString(obj.el)) {
+            throw new Error("Table传入的el参数必须是字符串");
+        }
+
+        var tableContainer = document.querySelector(obj.el);
+        if (typeUtil.isNull(tableContainer) || typeUtil.isUndefined(tableContainer)) {
+            throw new Error("Html选择器无法找到对应的元素对象");
+        }
+    };
+
+    return Table;
 });
